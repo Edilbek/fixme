95c14ecb9ecb44916873a1edb8e106fce056f916
Edilbek <edil.talantbekov@gmail.com>
2017-12-13 21:17:55 +0600

    add production action mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,8 +2,6 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.cache_classes = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -26,8 +24,6 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
 
@@ -52,6 +48,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
