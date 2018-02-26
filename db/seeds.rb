7607c244649d5e064ce5b868c44a31a2d6e6016d
Edilbek <edil.talantbekov@gmail.com>
2018-02-26 17:17:18 +0600

    remove unnecessary code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # Admin.create!(email: 'admin@admin', password: 'parol123', password_confirmation: 'parol123')
 
-environment_seed_file = File.join(Rails.root, 'db', 'seeds', "#{Rails.env}.rb")
+# environment_seed_file = File.join(Rails.root, 'db', 'seeds', "#{Rails.env}.rb")
 
 def seed_image(id)
   File.open(File.join(Rails.root, "/public/seeds/problem_#{id}.png"))
@@ -16,5 +16,4 @@ end
     status: rand(1..2),
     images: [ seed_image(rand(1..2)) ]
   )
-  # problem.create!(images: seed_image(rand(1..2)))
 end
