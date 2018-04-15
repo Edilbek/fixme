b1e9319faa661991809ce1dcf3a1914dca5fe01b
Edilbek <edil.talantbekov@gmail.com>
2018-04-15 15:45:53 +0600

    setting mailer in production

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,13 @@ require_relative 'application'
 require 'carrierwave/orm/activerecord'
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'edil.talantbekov@gmail.com',
+  :password => 'barcelona123',
+  :domain => 'fixme-edil.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
