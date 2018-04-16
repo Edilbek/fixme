dad288e7c442f76865cbb4b47d4477fbef15d1a3
Edilbek <edil.talantbekov@gmail.com>
2018-04-17 00:09:05 +0600

    setting heroku mailer

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => 'edil.talantbekov',
   :password => 'barcelona123',
-  :domain => 'heroku.com',
+  :domain => 'fixme-edil.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
