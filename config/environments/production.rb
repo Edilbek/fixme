84c8f5d2eaef89d468e42e2d2fb37ea789c231ae
Edilbek <edil.talantbekov@gmail.com>
2018-04-16 23:29:55 +0600

    setting smpt heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,10 +104,8 @@ Rails.application.configure do
   #   :authentication       => "plain",
   # }
   config.action_mailer.default_url_options = { :host => 'fixme-edil.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
