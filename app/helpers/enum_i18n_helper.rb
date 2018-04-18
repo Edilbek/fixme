549aa9bba851d3db9c1a178ed258fc23ff780602
Edilbek <edil.talantbekov@gmail.com>
2018-04-18 17:05:53 +0600

    locale model attributes name to russian and set up simple form

---
+++ app/helpers/enum_i18n_helper.rb
@@ -0,0 +1,23 @@
+module EnumI18nHelper
+
+  # Returns an array of the possible key/i18n values for the enum
+  # Example usage:
+  def enum_options_for_select(class_name, enum)
+    class_name.send(enum.to_s.pluralize).map do |key, _|
+      [enum_i18n(class_name, enum, key), key]
+    end
+  end
+
+  # Returns the i18n version the models current enum key
+  # Example usage:
+  def enum_l(model, enum)
+    enum_i18n(model.class, enum, model.send(enum))
+  end
+
+  # Returns the i18n string for the enum key
+  # Example usage:
+  def enum_i18n(class_name, enum, key)
+    I18n.t("activerecord.enums.#{class_name.model_name.i18n_key}.#{enum.to_s.pluralize}.#{key}")
+  end
+
+end
